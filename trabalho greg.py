1ba0bffa9d14ca5dbe3c577fef317a9889dbf2ce
dia2007 <dianinhasantana07@gmail.com>
2024-10-23 21:13:32 -0300

    Add files via upload

---
+++ trabalho greg.py
@@ -0,0 +1,9 @@
+#trabalho greg
+import pywhatkit as kit
+ 
+# Envie uma mensagem do WhatsApp para um contato às 13h30 
+kit.sendwhatmsg ( " " ,  "Olá, te adicionei a minha agenda de contatos" ,  20,  48 ) 
+# arumar o horario para mandar msg assim que adicionar o contato
+#arumar pra ir couquer numero
+
+kit.send_mail("dianinhasantana07@gmail.com", "dianasantanaalveschagas2007", "oii", "olaqaqa", "00001116639282sp@al.educacao.sp.gov.br")
